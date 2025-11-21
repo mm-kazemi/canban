9758094466c3cf2b53fdd861bf1ffc51933a02bf
mm-kazemi <mm.kazemi.work@gmail.com>
2025-11-21 20:32:19 +0330

    [ADD] app htl and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,75 @@
 import type { ReactNode } from "react";
 
-import "./App.css";
+import styles from "./App.module.css";
 
 function App(): ReactNode {
-  return <div>Hello World!</div>;
+  return (
+    <div className={styles.app}>
+      <header>Header</header>
+      <main>
+        <div className={styles.header}>
+          <h1>Boards</h1>
+          <button>Create</button>
+        </div>
+        <ul className={styles.boards}>
+          <li>
+            <div className={styles.board}>
+              <div className={styles.cover}></div>
+              <div className={styles.content}>
+                <div className={styles.header}>
+                  <div className={styles.title}>Board 1</div>
+                  <a href={"/board"}>View</a>
+                </div>
+                <p className={styles.description}>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  Alias amet, autem consequatur consequuntur cumque dolorum eos
+                  esse et harum itaque labore libero minima odit pariatur rem
+                  repellat sit vel vitae!
+                </p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className={styles.board}>
+              <div className={styles.cover}>
+                <div className={styles.content}>
+                  <div className={styles.header}>
+                    <div className={styles.title}>Board 1</div>
+                    <a href={"/board"}>View</a>
+                  </div>
+                  <p className={styles.description}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Alias amet, autem consequatur consequuntur cumque dolorum
+                    eos esse et harum itaque labore libero minima odit pariatur
+                    rem repellat sit vel vitae!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div className={styles.board}>
+              <div className={styles.cover}>
+                <div className={styles.content}>
+                  <div className={styles.header}>
+                    <div className={styles.title}>Board 1</div>
+                    <a href={"/board"}>View</a>
+                  </div>
+                  <p className={styles.description}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Alias amet, autem consequatur consequuntur cumque dolorum
+                    eos esse et harum itaque labore libero minima odit pariatur
+                    rem repellat sit vel vitae!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </main>
+      <footer>Footer</footer>
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
 import "./index.css";
+import "./styles/colors.css";
+import "./styles/shadow.css";
+import "./styles/shapes.css";
+import "./styles/typography.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
