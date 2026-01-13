603ec72f358ec08ebaab86862cbc0b3885ef8ffe
mm.kazemi <mm.kazemi.work@gmail.com>
2026-01-13 14:24:31 +0330

    [ADD] color input

---
+++ src/components/ColorInput/ColorInput.tsx
@@ -0,0 +1,67 @@
+import { type ComponentProps, type ReactNode, useId, useState } from "react";
+
+import clsx from "clsx";
+
+import { BOARD_COLORS, type BoardColor } from "../../types/board.ts";
+
+import styles from "./ColorInput.module.css";
+
+type Props = Omit<
+  ComponentProps<"input">,
+  "type" | "value" | "defaultValue" | "onChange"
+> & {
+  label: string;
+  value?: BoardColor;
+  defaultValue?: BoardColor;
+  error?: string | null;
+  onChange?: (value?: BoardColor) => void;
+};
+
+function ColorInput({
+  className,
+  label,
+  value: controlledValue,
+  defaultValue,
+  onChange,
+  error,
+  ...otherProps
+}: Props): ReactNode {
+  const [uncontrolledValue, setUncontrolledValue] = useState<BoardColor>(
+    defaultValue ?? "blue",
+  );
+
+  const value = controlledValue ?? uncontrolledValue;
+
+  const id = useId();
+
+  const handleButtonClick = (color: BoardColor): void => {
+    setUncontrolledValue(color);
+    onChange?.(color);
+  };
+
+  return (
+    <div
+      className={clsx(
+        styles["color-input"],
+        !!error && styles.error,
+        className,
+      )}
+    >
+      <label htmlFor={id}>{label}</label>
+      <div className={styles.colors}>
+        {BOARD_COLORS.map((color) => (
+          <button
+            key={color}
+            className={clsx(color, color === value && styles.active)}
+            type={"button"}
+            onClick={() => handleButtonClick(color)}
+          ></button>
+        ))}
+      </div>
+      <input id={id} type={"hidden"} value={value} {...otherProps} />
+      <span className={styles.error}>{error || "\u00A0"}</span>
+    </div>
+  );
+}
+
+export default ColorInput;
