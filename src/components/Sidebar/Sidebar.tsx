feab8056ba60f99025b3bc7cbc5b12991d9ed351
mm-kazemi <mm.kazemi.work@gmail.com>
2026-02-15 01:31:52 +0330

    [ADD] sidebar component

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,31 @@
+import type { ReactNode } from "react";
+
+import { Link } from "react-router";
+
+import SidebarGroups from "../../components/Sidebar/components/SidebarGroups/SidebarGroups.tsx";
+import MingcuteExitLine from "../../icons/MingcuteExitLine.tsx";
+import SidebarItem from "./components/SidebarItem/SidebarItem.tsx";
+
+import styles from "./Sidebar.module.css";
+
+export default function Sidebar(): ReactNode {
+  return (
+    <aside className={styles.sidebar}>
+      <div className={styles.header}>
+        <Link className={styles.logo} to="/">
+          <img src="/logo.svg" alt="Canban Logo" />
+        </Link>
+      </div>
+      <nav>
+        <SidebarGroups />
+      </nav>
+      <div className={styles.footer}>
+        <SidebarItem
+          title="Sign Out"
+          color="gray"
+          icon={<MingcuteExitLine />}
+        />
+      </div>
+    </aside>
+  );
+}
--- src/layouts/RootLayout/RootLayout.tsx
+++ src/layouts/RootLayout/RootLayout.tsx
@@ -1,25 +1,23 @@
-import { type ReactNode } from "react";
+import type { ReactNode } from "react";
 
 import { Outlet } from "react-router";
 
 import Footer from "../../components/Footer/Footer.tsx";
-import Header from "../../components/Header/Header.tsx";
+import Sidebar from "../../components/Sidebar/Sidebar.tsx";
 import BoardsProvider from "../../providers/BoardsProvider.tsx";
 
 import styles from "./RootLayout.module.css";
 
-function RootLayout(): ReactNode {
+export default function RootLayout(): ReactNode {
   return (
     <BoardsProvider>
       <div className={styles["root-layout"]}>
-        <Header />
         <main>
           <Outlet />
         </main>
+        <Sidebar />
         <Footer />
       </div>
     </BoardsProvider>
   );
 }
-
-export default RootLayout;
